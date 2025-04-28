f9659a79f23e0ad7d7d2907445ccc5f2afa919ec
Ultra-MG <malihassaad100@gmail.com>
2025-04-28 09:55:23 -0400

    malih

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCn_CKAdsJ1v0NLNl6LwPiMXg_r5xZGv4I",
-  authDomain: "bidifymobile.firebaseapp.com",
-  projectId: "bidifymobile",
-  storageBucket: "bidifymobile.firebasestorage.app", // ✅ FIXED
-  messagingSenderId: "969454814980",
-  appId: "1:969454814980:web:97e944b01d37d59c6b3251",
-  measurementId: "G-JX9H7HG38Y"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: ".firebasestorage.app", 
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // ✅ prevent duplicate initialization
